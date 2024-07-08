ce34ad0f23b95f9676979b192cc44106fa2269ed
AndreasHolmAkselsen <andreas.akselsen@sintef.no>
2024-07-08 11:16:49 +0200

    .

---
+++ multi_axes2.m
@@ -0,0 +1,46 @@
+function [h_ax,h_fig] = multi_axes2(n,m,h_fig,margin,sep)
+% multi_axes(n, m, h_fig, margin, sep), 
+% margin = [dxLeft, dyBottom, dxRight, dyTop], 
+% sep = [sep_x,sep_y]
+
+if exist('h_fig','var') && ~isempty(h_fig)
+    figure(h_fig);
+else
+    h_fig = figure;
+    h_fig.Color = 'white';
+    h_fig.Position = [0         724        1586        1111];
+end
+
+if ~exist('margin','var') || isempty(margin)
+    margin = [.05,.05,.04,.05];
+end
+margin_xL = margin(1);
+margin_yB = margin(2);
+margin_xR = margin(3);
+margin_yT = margin(4);
+
+
+if exist('sep','var') && ~isempty(sep)
+    sep_x = sep(1);
+    sep_y = sep(2);
+else  
+    sep_x = .05;
+    sep_y = .05;
+end
+
+width =  (1-margin_xL-margin_xR-(m-1)*sep_x)/m;
+height = (1-margin_yB-margin_yT-(n-1)*sep_y)/n;
+
+for i = n:-1:1
+    for j = m:-1:1
+        h_ax(n-i+1,j) = axes('Position', ...
+            [margin_xL+(j-1)*(width +sep_x),...
+             margin_yB+(i-1)*(height+sep_y),...
+             width, height]);
+         hold on
+    end
+end
+
+% for i = 1:n,  for j = 1:m, axes(h_ax(i,j)); text(.5,.5,num2str([i,j])); end; end
+
+end
---
+++ positionFigures.m
@@ -0,0 +1,62 @@
+function hfs = positionFigures(hfs,screenFrac,posArray,screenNumber,sortFigures)
+% Spreads figures out across screen.
+% hfs = positionFigures(); 
+% hfs = positionFigures(hfs,screenFrac,posArray,screenNumber,sortFigures);
+%
+%   INPUT
+%   hfs:          Figure handle array. Default: all available figures. 
+%   screenFrac:   [fx,fy,fwx,fwy]. Fraction of screen in which to place figures. Default: [1,1,1,1].
+%   posArray:     [nx,ny]. Number of figures in each direction. Default: square distribution.
+%   screenNumber: Integer. Monitor number in case there are several. Default: 1.
+%   sortFigures:  Boolean. Whether to sort figures after figure number. Default: true.
+%
+% AHA, June 2024.
+
+
+% set defaults
+if nargin<1 || isempty(hfs)
+    hfs = findall(groot,'Type','figure');
+end
+if nargin<2 || isempty(screenFrac)
+    screenFrac = [1,1,1,1];
+end
+if nargin<3 || isempty(screenFrac)
+    posArray = [];
+end
+if nargin<4 || isempty(screenNumber)
+    screenNumber = 1;
+end
+if nargin<5 || isempty(sortFigures)
+    sortFigures = true;
+end
+
+hfs(~isvalid(hfs)) = []; % remove handles to deleted fuigures
+if sortFigures
+    [~,ii] = sort([hfs.Number]); hfs = hfs(ii);
+end
+
+% set position array
+n = numel(hfs);
+if isempty(posArray)
+    % square division
+    nx = ceil(sqrt(n));
+    ny = ceil(n/nx);
+else
+    nx = posArray(1); ny = posArray(2);
+    assert(nx*ny>=n,'Input position array to small to host all figures.')
+end
+
+% position figures
+monPos = get(0).MonitorPositions(screenNumber,:);
+figArea = monPos.*screenFrac;
+wx = figArea(3)/nx; wy = figArea(4)/ny;
+x = figArea(1); y = figArea(2)+figArea(4)-wy; 
+for i = 1:length(hfs)
+    hfs(i).Position = [x,y,wx,wy];
+    x = x+wx;
+    if x>= figArea(1)+figArea(3)
+        x = figArea(1);
+        y = y-wy;
+    end
+    figure(hfs(i));
+end
