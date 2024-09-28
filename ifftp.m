a7572d234e611e99ee317fc92e01f16252729a45
Andreas Holm Akselsen <andreas.akselsen@sintef.no>
2024-09-28 11:43:45 +0200

    Function for clean ifft padding.

---
+++ ifftp.m
@@ -0,0 +1,27 @@
+function x = ifftp(hx,np,varargin)
+% x = ifftp(hx,np)
+% x = ifftp(hx,np,'shxmmetric')
+% Inverse fft with padding/trimming such that size(x,1) = np.
+% Supports both odd and even length input.
+% Assumes zero-mode at first index.
+%
+% AHA, Spe. 2024.
+
+siz = size(hx);
+n = siz(1);
+m = prod(siz(2:end));
+if np >= n
+    if mod(n,2)==0
+        hx = [hx(1:n/2,:);.5*hx(n/2+1,:);zeros(np-n-1,m);.5*hx(n/2+1,:);hx(n/2+2:n,:)];
+    else
+        hx = [hx(1:(n+1)/2,:);zeros(np-n,m);hx((n+1)/2+1:n,:)];
+    end
+else
+    if mod(np,2)==0
+        hx = [hx(1:np/2,:);2*real(hx(n-np/2+1,:));hx(n-np/2+2:n,:)]; 
+    else
+        hx = [hx(1:(np+1)/2,:);hx(n-(np+1)/2+2:n,:)]; 
+    end
+end
+hx = reshape(hx,[np,siz(2:end)]);
+x = ifft(hx,[],varargin{:});
--- sqifft.m
+++ sqifft.m
@@ -1,8 +1,14 @@
-function x = sqifft(hx2)
+function x = sqifft(hx2,nx)
 %
 % computes double sum 
 %   x = sum_{n=-0}^{N-1} sum_{m=-0}^{N-1}  hx2.*exp(2i*pi*( n+m))/M
 % where M = 2*N-1.
+%
+% Input:
+%   hx2: NxN matrix of complex Fourier amplitudes, zero-frequencies at first indiex of each dimension.
+%   nx (optional): scalar number of output points (resolution). Default: nx = 2*N-1, which is the lowest 
+%                  resolution maintaining all information. if nx < 2*N-1 then nx should be odd. 
+%
 % for example
 %   x = sum( fft(f).*fft(g.').*exp(1i*(wf+wg)*t), [1,2])
 % where
@@ -13,13 +19,10 @@ function x = sqifft(hx2)
 % AHA, Sep 2024
 
 
-[N,M] = size(hx2);
-assert(N==M,'Input matrix must be square.');
-
-% x = checkReal( ifft(ifftshift(sumAntiDiag(fftshift(hx2))) ) );
-x = ifft(ifftshift(sumAntiDiag(fftshift(hx2))),[],'symmetric');
-
-
+assert(ismatrix(hx2),diff(size(hx2))==0,'Input matrix must be square.');
+hxDiag = ifftshift(sumAntiDiag(fftshift(hx2)));
+% x = checkReal( ifftp(hxDiag,nx) );
+x = ifftp(hxDiag,nx,'symmetric');
 end
 
 
@@ -34,3 +37,27 @@ function Asum = sumAntiDiag(A)
         Asum(nn) = Asum(nn) + A(:,n);
     end
 end
+
+
+
+
+
+
+
+% [N,M] = size(hx2);
+% assert(N==M,'Input matrix must be square.');
+% hxDiag = ifftshift(sumAntiDiag(fftshift(hx2)));
+% if nargin >= 2 % pad
+%     nDiag = 2*N-1;
+%     if nx < nDiag
+%         warning('Trimming under-resolved signal. nx=%d, nDiag = %d.',nx,nDiag);
+%         assert(mod(nx,2)==1,'under-resolved nx should be odd')
+%         hxDiag = [hxDiag(1:(nx+1)/2);hxDiag(nDiag-(nx-3)/2:nDiag)];
+%     else
+%         hxDiag = [hxDiag(1:N);zeros(nx-nDiag,1);hxDiag(N+1:nDiag)];
+%     end
+%     assert(numel(hxDiag)==nx);
+% end
+% 
+% x = checkReal( ifft(hxDiag) );
+% % x = ifft(hxDiag,[],'symmetric');
